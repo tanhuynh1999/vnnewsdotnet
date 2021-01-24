38384decda0f7e9c278ce4c6fd2cb7edc88e4a71
Tấn Thanh <tanthanh141214@gmail.com>
2021-01-24 21:08:15 +0700

    account

--- vnnews/Controllers/ViewController.cs
+++ vnnews/Controllers/ViewController.cs
@@ -3,15 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vnnews.Models;
 
 namespace vnnews.Controllers
 {
     public class ViewController : Controller
     {
+        vnnewsEntities db = new vnnewsEntities();
         // GET: View
         public PartialViewResult Menu()
         {
             return PartialView();
+        }
+        public PartialViewResult SignUp()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult SignUp(ViewSignUp signUp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(new User
+                {
+                    user_name = signUp.DisplayName,
+                    user_email = signUp.Email,
+                    user_pass = signUp.Password,
+                    user_bin = false,
+                    user_active = true,
+                    role_id = 1,
+                    user_datecreate = DateTime.Now,
+                    user_datelogin = DateTime.Now
+                });
+                db.SaveChanges();
+
+                User user = db.Users.SingleOrDefault(t => t.user_email == signUp.Email && t.user_pass == signUp.Password && t.user_bin == false && t.user_active == true);
+                HttpCookie cookie = new HttpCookie("user_cookie", user.user_id.ToString());
+                cookie.Expires.AddDays(10);
+                Response.SetCookie(cookie);
+            }
+            return Redirect(signUp.ReturnUrl);
+        }
+
+        public PartialViewResult SignIn()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult SignIn(ViewSignIn signIn)
+        {
+            User user = db.Users.SingleOrDefault(t => t.user_email == signIn.Email && t.user_pass == signIn.Password && t.user_bin == false && t.user_active == true);
+            if(user != null)
+            {
+                HttpCookie cookie = new HttpCookie("user_cookie", user.user_id.ToString());
+                cookie.Expires.AddDays(10);
+                Response.SetCookie(cookie);
+            }
+            return Redirect(signIn.ReturnUrl);
         }
     }
 }
---
+++ vnnews/Models/ViewAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vnnews.Models
+{
+    public class ViewSignUp
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPass { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+
+    public class ViewSignIn
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}

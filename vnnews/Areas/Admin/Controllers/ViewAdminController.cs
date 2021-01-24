d091bed1b77377b61d995b12a6fa702796deea8b
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-01-24 18:26:22 +0700

    Add project files.

---
+++ vnnews/Areas/Admin/Controllers/ViewAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vnnews.Areas.Admin.Controllers
+{
+    public class ViewAdminController : Controller
+    {
+        // GET: Admin/ViewAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ vnnews/Controllers/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vnnews.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public PartialViewResult Menu()
+        {
+            return PartialView();
+        }
+    }
+}
